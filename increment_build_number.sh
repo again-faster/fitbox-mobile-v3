e21c505a782b4be6f660f9d78b0954e70c91116e
Lee Alden <bleealden@gmail.com>
2024-06-21 10:17:46 +0800

    build: script changes

--- increment_build_number.sh
+++ increment_build_number.sh
@@ -5,10 +5,11 @@ INFO_PLIST_PATH="ios/fitbox/Info.plist"  # Adjust path as per your project struc
 
 # Read current build number
 CURRENT_PROJECT_VERSION=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "$INFO_PLIST_PATH")
+echo "Current Project Version: $CURRENT_PROJECT_VERSION"
 
 # Increment build number
 NEW_PROJECT_VERSION=$((CURRENT_PROJECT_VERSION + 1))
-
+echo "New Project Version (1): $NEW_PROJECT_VERSION"
 # Update Info.plist with new build number
 /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $NEW_PROJECT_VERSION" "$INFO_PLIST_PATH"
 
@@ -17,8 +18,8 @@ echo "New Project Version: $NEW_PROJECT_VERSION"
 echo "::set-output name=new_project_version::$NEW_PROJECT_VERSION"
 
 # Commit the updated Info.plist if there are changes
-if [ -n "$(git status --porcelain "$INFO_PLIST_PATH")" ]; then
-    git add "$INFO_PLIST_PATH"
-    git commit -m "build: increment project version to $NEW_PROJECT_VERSION" --author="GitHub Actions <actions@github.com>"
-    git push origin HEAD:feat/automate-build-number-2  # change to which branch you are pushing the changes
-fi
+# if [ -n "$(git status --porcelain "$INFO_PLIST_PATH")" ]; then
+#     git add "$INFO_PLIST_PATH"
+#     git commit -m "build: increment project version to $NEW_PROJECT_VERSION" --author="GitHub Actions <actions@github.com>"
+#     git push origin HEAD:feat/automate-build-number-2  # change to which branch you are pushing the changes
+# fi
