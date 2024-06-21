39c705ca3bf2462a20c82f0467e8835e145ebffb
Lee Alden <bleealden@gmail.com>
2024-06-21 09:41:37 +0800

    build: script changes

--- increment_build_number.sh
+++ increment_build_number.sh
@@ -4,21 +4,21 @@
 INFO_PLIST_PATH="ios/fitbox/Info.plist"  # Adjust path as per your project structure
 
 # Read current build number
-CURRENT_BUILD_NUMBER=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "$INFO_PLIST_PATH")
+CURRENT_PROJECT_VERSION=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "$INFO_PLIST_PATH")
 
 # Increment build number
-NEW_BUILD_NUMBER=$((CURRENT_BUILD_NUMBER + 1))
+NEW_PROJECT_VERSION=$((CURRENT_PROJECT_VERSION + 1))
 
 # Update Info.plist with new build number
-/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $NEW_BUILD_NUMBER" "$INFO_PLIST_PATH"
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $NEW_PROJECT_VERSION" "$INFO_PLIST_PATH"
 
 # Print and export the new build number for subsequent steps to use
-echo "New Build Number: $NEW_BUILD_NUMBER"
-echo "::set-output name=new_build_number::$NEW_BUILD_NUMBER"
+echo "New Project Version: $NEW_PROJECT_VERSION"
+echo "::set-output name=new_project_version::$NEW_PROJECT_VERSION"
 
 # Commit the updated Info.plist if there are changes
 if [ -n "$(git status --porcelain "$INFO_PLIST_PATH")" ]; then
     git add "$INFO_PLIST_PATH"
-    git commit -m "Increment build number to $NEW_BUILD_NUMBER"
-    git push origin main  # Push the commit to your repository
+    git commit -m "build: increment project version to $NEW_PROJECT_VERSION" --author="GitHub Actions <actions@github.com>"
+    git push origin HEAD:main  # Push the commit to your repository's main branch
 fi
