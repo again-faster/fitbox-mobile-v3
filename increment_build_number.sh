b6762ed51971cb39a51961dca0aa289c23a7caa9
Lee Alden <bleealden@gmail.com>
2024-06-21 12:01:44 +0800

    build: script changes

--- increment_build_number.sh
+++ increment_build_number.sh
@@ -26,7 +26,7 @@ echo "New Project Version (1): $NEW_PROJECT_VERSION"
 # Update Info.plist with new build number
 # /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $NEW_PROJECT_VERSION" "$INFO_PLIST_PATH"
 
-sed -i '' -e "s/CURRENT_PROJECT_VERSION = [0-9]*;/CURRENT_PROJECT_VERSION = $NEW_PROJECT_VERSION;/g" "ios/fitbox/xcodeproj"
+sed -i '' -e "s/CURRENT_PROJECT_VERSION = [0-9]*;/CURRENT_PROJECT_VERSION = $NEW_PROJECT_VERSION;/g" "ios/fitbox.xcodeproj"
 
 echo "Updated CURRENT_PROJECT_VERSION to $CURRENT_PROJECT_VERSION in THIS"
 
@@ -35,8 +35,8 @@ echo "New Project Version: $NEW_PROJECT_VERSION"
 echo "::set-output name=new_project_version::$NEW_PROJECT_VERSION"
 
 # Commit the updated Info.plist if there are changes
-# if [ -n "$(git status --porcelain "$INFO_PLIST_PATH")" ]; then
-#     git add "$INFO_PLIST_PATH"
-#     git commit -m "build: increment project version to $NEW_PROJECT_VERSION" --author="GitHub Actions <actions@github.com>"
-#     git push origin HEAD:feat/automate-build-number-2  # change to which branch you are pushing the changes
-# fi
+if [ -n "$(git status --porcelain "$INFO_PLIST_PATH")" ]; then
+    git add "$INFO_PLIST_PATH"
+    git commit -m "build: increment project version to $NEW_PROJECT_VERSION" --author="GitHub Actions <actions@github.com>"
+    git push origin HEAD:feat/automate-build-number-2  # change to which branch you are pushing the changes
+fi
