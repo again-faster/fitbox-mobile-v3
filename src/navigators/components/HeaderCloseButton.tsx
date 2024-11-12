f832628062ed35c18c3169f203bb65ecf5eb5a87
Karl Dale Arriba <34635589+karl007253@users.noreply.github.com>
2024-11-12 10:31:17 +0800

    fix: missing color for header close button

--- src/navigators/components/HeaderCloseButton.tsx
+++ src/navigators/components/HeaderCloseButton.tsx
@@ -1,11 +1,17 @@
 import HeaderButtonGroup from '@/components/template/Header/HeaderButtonGroup';
+import { config } from '@/theme/_config';
 import Ionicons from 'react-native-vector-icons/MaterialCommunityIcons';
 import { goBack } from '../NavigationRef';
 
 const HeaderCloseButton = () => {
 	return (
 		<HeaderButtonGroup>
-			<Ionicons name="close" size={24} onPress={() => goBack()} />
+			<Ionicons
+				name="close"
+				size={24}
+				onPress={() => goBack()}
+				color={config.fonts.colors.darkgray}
+			/>
 		</HeaderButtonGroup>
 	);
 };
